62aad4fd15f84fbc9f0c9d121d8955a5d32352ba
lostroji <rojinab78@gmail.com>
2026-06-20 20:42:00 +0330

    Add description

--- 6-kyu/help-the-bookseller/src/main/kotlin/Main.kt
+++ 6-kyu/help-the-bookseller/src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+// ===================================================================
+// Kata: Help The Bookseller
+// Rank: 6 kyu
+// Link: https://www.codewars.com/kata/54dc6f5a224c26032800005c/kotlin
+// Date: 2026-06-20
+// ===================================================================
+
 package solution
 
 object StockList {
